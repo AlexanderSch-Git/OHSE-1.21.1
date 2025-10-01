ca5791c9ac2974821d5ab5f41f38ebf949c5b8b2
SirHephaistos <sirhephaistos@gmail.com>
2025-10-01 21:24:11 +0200

    Initial commit

---
+++ src/main/java/as/sirhephaistos/ohse/OverhauledHordeSpawnEngine.java
@@ -0,0 +1,31 @@
+package as.sirhephaistos.ohse;
+
+import as.sirhephaistos.ohse.registry.OHSEItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OverhauledHordeSpawnEngine implements ModInitializer {
+	public static final String MOD_ID = "ohse";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+        LOGGER.info("[OHSE]Overhauled Horde Spawn Engine initialisation.");
+        // Register items
+        OHSEItems.register();
+        LOGGER.info("[OHSE]Items registered.");
+        // Register commands
+        OHSEItems.register();
+        LOGGER.info("[OHSE]Commands registered.");
+        LOGGER.info("[OHSE]Overhauled Horde Spawn Engine initialized.");
+	}
+}
---
+++ src/main/java/as/sirhephaistos/ohse/registry/OHSEItems.java
@@ -0,0 +1,44 @@
+package as.sirhephaistos.ohse.registry;
+
+
+import as.sirhephaistos.ohse.world.item.ZoneWandItem;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+/**
+ * Item registry for OHSE.
+ * Keeps static references to mod items.
+ */
+public final class OHSEItems {
+    public static final String MOD_ID = "ohse";
+
+    public static Item ZONE_WAND;
+
+    private OHSEItems() {
+    }
+
+    public static void register() {
+        ZONE_WAND = registerWand("zone_wand");
+
+        // Creative tab (dev)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+                .register(entries -> entries.add(ZONE_WAND));
+    }
+
+    private static Item registerWand(String path) {
+        // 1. Create the item normally
+        Item item = new ZoneWandItem(new Item.Settings().maxCount(1));
+        // 2. Register with an Identifier (NOT RegistryKey)
+        return Registry.register(
+                Registries.ITEM,
+                Identifier.of(MOD_ID, path),
+                item
+        );
+    }
+}
